1cae9cc966b610f15643aa8f5ec05f92c2aca2d4
Benoitdw <bw@oncodna.com>
2022-01-28 14:26:35 +0100

    remove unwanted directory

--- build/lib/db_connector/db_handler_config.py
+++
@@ -1,9 +0,0 @@
-from schematics.models import Model
-from schematics.types import StringType
-
-
-class DbAccess(Model):
-    host = StringType(required=True)
-    user = StringType(required=True)
-    password = StringType(required=True)
-    table = StringType(required=True)
