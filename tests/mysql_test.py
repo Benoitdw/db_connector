e4a874bd0e1c82c104e781e18f2d81090ebec2c2
Benoitdw <bw@oncodna.com>
2022-02-21 13:44:10 +0100

    Add insert test
    add insert tests

--- tests/mysql_test.py
+++ tests/mysql_test.py
@@ -12,3 +12,14 @@ class TestMysql:
     def test_many(self):
         nb = 5
         assert len(self.skywalker_handler.fetchmany(self.query, nb)) == nb
+
+    def test_insert_one(self):
+        query = "INSERT INTO TASK_STATE (task_id, analysis_name, task_name, task_state) VALUES (%s, %s, %s, %s)"
+        values = [('test', '02233', 'test', 'completed')]
+        self.skywalker_handler.insert_value(query, values)
+
+    def test_insert_multiple(self):
+        query = "INSERT INTO TASK_STATE (task_id, analysis_name, task_name, task_state) VALUES (%s, %s, %s, %s)"
+        values = [('test', '02233', 'test', 'completed'),
+                  ('test2', '02233', 'test2', 'failed')]
+        self.skywalker_handler.insert_value(query, values)
